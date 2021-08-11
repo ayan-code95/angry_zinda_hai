8b393f79bc04560043945d94856a7b9324c9e5a1
ayan-code95 <mushrrat.ldh42@gmail.com>
2021-08-11 18:12:49 +0530

    hb

--- Slingshot.js
+++ Slingshot.js
@@ -26,7 +26,15 @@ class SlingShot{
             var pointA = this.Slingshot.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x<220){
+            line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+            image(this.sling3,pointA.x-30, pointA.y-10, 10,30)
+            } else{
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y); 
+                image(this.sling3,pointA.x+20, pointA.y-10, 10,30) 
+            }
 
            
             
